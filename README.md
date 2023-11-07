f80a82309067bd0485e61525a3009153bac7d0aa
ddlpr <103121607+ddlpr@users.noreply.github.com>
2023-11-07 14:18:25 -0800

    Add record editing functionality
    Refactor insert operation and implement record editing

---
+++ README.md
@@ -0,0 +1,1 @@
+# Formulario de películas
--- main.js
+++ main.js
@@ -1,42 +1,67 @@
 const form = document.querySelector('#formulario');
-const inputFirstName = document.querySelector('#nombre');
-const inputLastName = document.querySelector('#apellido');
-const inputEmail = document.querySelector('#correo');
-const inputAge = document.querySelector('#edad');
-const inputHobbies = document.querySelector('#hobbies');
+const inputTitle = document.getElementById("titulo");
+const inputYear = document.getElementById("anio");
+const inputDuration = document.getElementById("duracion");
+const inputGenre = document.getElementById("genero");
+const inputDirector = document.getElementById("director");
+const textAreaSynopsis = document.getElementById("sinopsis");
+const table = document.querySelector('.table');
+
+let editingRow = null;
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  let student = {
-    name: inputFirstName.value,
-    lastname: inputLastName.value,
-    email: inputEmail.value,
-    age: inputAge.value,
-    hobbies: inputHobbies.value,
-  };
+  let formData = getFormData();
 
-  addTable(student);
+  if (!editingRow) {
+    insertRecord(formData);
+  } else {
+    updateRecord(formData)
+  }
   e.target.reset();
+  editingRow = null;
 });
 
-function addTable(student) {
-  let tbody = document.querySelector('#tableBody')
-  let row = document.createElement('tr');
+function getFormData() {
+  return {
+    titulo: document.querySelector('#titulo').value,
+    anio: document.querySelector('#anio').value,
+    duracion: document.querySelector('#duracion').value,
+    genero: document.querySelector('#genero').value,
+    director: document.querySelector('#director').value,
+    sinopsis: document.querySelector('#sinopsis').value,
+  };
+}
 
-  for (let key in student) {
-    let cell = document.createElement('td');
-    cell.textContent = student[key];
-    row.append(cell);
+function insertRecord(formData) {
+  let row = table.insertRow();
+  for (const key in formData) {
+    let cell = row.insertCell();
+    cell.textContent = formData[key];
   }
+  let cell = row.insertCell();
+  cell.innerHTML = `<button type="button" class="btn btn-danger"><i class="bi bi-trash3-fill"></i></button>`;
 
-  let td = document.createElement('td');
-  let button = document.createElement ('button');
-  button.textContent = "Eliminar";
-  button.onclick = function() {
-    tbody.remove(row);
-  }
-  td.append(button);
-  row.append(td)
-  tbody.append(row);
+  cell = row.insertCell();
+  cell.innerHTML = `<button type="button" class="btn btn-success" onclick="fillEditForm(${row.rowIndex})"><i class="bi bi-pencil-fill"></i></button>`;
+}
+
+function fillEditForm(rowIndex) {
+  editingRow = table.rows[rowIndex];
+  inputTitle.value = editingRow.children[0].textContent;
+  inputYear.value = editingRow.children[1].textContent;
+  inputDuration.value = editingRow.children[2].textContent;
+  inputGenre.value = editingRow.children[3].textContent;
+  inputDirector.value = editingRow.children[4].textContent;
+  textAreaSynopsis.value = editingRow.children[5].textContent;
+}
+
+function updateRecord(formData) {
+  editingRow.children[0].textContent = formData.titulo;
+  editingRow.children[1].textContent = formData.anio;
+  editingRow.children[2].textContent = formData.duracion;
+  editingRow.children[3].textContent = formData.genero;
+  editingRow.children[4].textContent = formData.director;
+  editingRow.children[5].textContent = formData.sinopsis;
 }
